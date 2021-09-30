296df8bbba43be175928999fd0a180664b62c434
lewisfelix <luisfgon@it.uc3m.es>
2021-09-30 10:03:18 +0000

    Adding fishy-domain-2 config files

---
+++ fishy-domain-2/config_interfaces.bash
@@ -0,0 +1,60 @@
+ip link add dev vdatapod1 type veth peer name vdatahost1
+ip link add dev vdatapod2 type veth peer name vdatahost2
+ip link add dev vdatapod3 type veth peer name vdatahost3
+ip link add dev vdatapod4 type veth peer name vdatahost4
+ip link add dev vdatapod5 type veth peer name vdatahost5
+ip link add dev vdatapod6 type veth peer name vdatahost6
+ip link add dev vdatapod7 type veth peer name vdatahost7
+ip link add dev vdatapod8 type veth peer name vdatahost8
+ip link add dev vdatapod9 type veth peer name vdatahost9
+ip link add dev vdatapod10 type veth peer name vdatahost10
+ip link add dev vctrlpod1 type veth peer name vctrlhost1
+ip link add dev vctrlpod2 type veth peer name vctrlhost2
+ip link add dev vctrlpod3 type veth peer name vctrlhost3
+ip link add dev vctrlpod4 type veth peer name vctrlhost4
+ip link add dev vctrlpod5 type veth peer name vctrlhost5
+ip link add dev vctrlpod6 type veth peer name vctrlhost6
+ip link add dev vctrlpod7 type veth peer name vctrlhost7
+ip link add dev vctrlpod8 type veth peer name vctrlhost8
+ip link add dev vctrlpod9 type veth peer name vctrlhost9
+ip link add dev vctrlpod10 type veth peer name vctrlhost10
+ip link set mtu 1450 dev vdatahost1
+ip link set mtu 1450 dev vdatapod1
+ip link set mtu 1450 dev vdatahost2
+ip link set mtu 1450 dev vdatapod2
+ip link set mtu 1450 dev vdatahost3
+ip link set mtu 1450 dev vdatapod3
+ip link set mtu 1450 dev vdatahost4
+ip link set mtu 1450 dev vdatapod4
+ip link set mtu 1450 dev vdatahost5
+ip link set mtu 1450 dev vdatapod6
+ip link set mtu 1450 dev vdatahost6
+ip link set mtu 1450 dev vdatapod6
+ip link set mtu 1450 dev vdatahost7
+ip link set mtu 1450 dev vdatapod7
+ip link set mtu 1450 dev vdatahost8
+ip link set mtu 1450 dev vdatapod8
+ip link set mtu 1450 dev vdatahost9
+ip link set mtu 1450 dev vdatapod9
+ip link set mtu 1450 dev vdatahost10
+ip link set mtu 1450 dev vdatapod10
+ip link set mtu 1450 dev vctrlhost1
+ip link set mtu 1450 dev vctrlpod1
+ip link set mtu 1450 dev vctrlhost2
+ip link set mtu 1450 dev vctrlpod2
+ip link set mtu 1450 dev vctrlhost3
+ip link set mtu 1450 dev vctrlpod3
+ip link set mtu 1450 dev vctrlhost4
+ip link set mtu 1450 dev vctrlpod4
+ip link set mtu 1450 dev vctrlhost5
+ip link set mtu 1450 dev vctrlpod5
+ip link set mtu 1450 dev vctrlhost6
+ip link set mtu 1450 dev vctrlpod6
+ip link set mtu 1450 dev vctrlhost7
+ip link set mtu 1450 dev vctrlpod7
+ip link set mtu 1450 dev vctrlhost8
+ip link set mtu 1450 dev vctrlpod8
+ip link set mtu 1450 dev vctrlhost9
+ip link set mtu 1450 dev vctrlpod9
+ip link set mtu 1450 dev vctrlhost10
+ip link set mtu 1450 dev vctrlpod10
