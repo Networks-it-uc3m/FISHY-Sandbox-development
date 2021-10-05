018f6c3b6ad77bc69bdfcf0dc14aa186f589f044
lewisfelix <luisfgon@it.uc3m.es>
2021-10-05 15:31:33 +0000

    Change mgmt terminology

--- fishy-control-services/config_interfaces.bash
+++ fishy-control-services/config_interfaces.bash
@@ -1,9 +1,30 @@
-ip link add dev vpodtm type veth peer name vhosttm
-ip link set mtu 1450 dev vpodtm
-ip link set mtu 1450 dev vhosttm
-ip link add dev vpodscm type veth peer name vhostscm
-ip link set mtu 1450 dev vpodscm
-ip link set mtu 1450 dev vhostscm
-ip link add dev vpodiro type veth peer name vhostiro
-ip link set mtu 1450 dev vpodiro
-ip link set mtu 1450 dev vhostiro
+ip link add dev vmgmtpod1 type veth peer name vmgmthost1
+ip link add dev vmgmtpod2 type veth peer name vmgmthost2
+ip link add dev vmgmtpod3 type veth peer name vmgmthost3
+ip link add dev vmgmtpod4 type veth peer name vmgmthost4
+ip link add dev vmgmtpod5 type veth peer name vmgmthost5
+ip link add dev vmgmtpod6 type veth peer name vmgmthost6
+ip link add dev vmgmtpod7 type veth peer name vmgmthost7
+ip link add dev vmgmtpod8 type veth peer name vmgmthost8
+ip link add dev vmgmtpod9 type veth peer name vmgmthost9
+ip link add dev vmgmtpod10 type veth peer name vmgmthost10
+ip link set mtu 1450 dev vmgmthost1
+ip link set mtu 1450 dev vmgmtpod1
+ip link set mtu 1450 dev vmgmthost2
+ip link set mtu 1450 dev vmgmtpod2
+ip link set mtu 1450 dev vmgmthost3
+ip link set mtu 1450 dev vmgmtpod3
+ip link set mtu 1450 dev vmgmthost4
+ip link set mtu 1450 dev vmgmtpod4
+ip link set mtu 1450 dev vmgmthost5
+ip link set mtu 1450 dev vmgmtpod5
+ip link set mtu 1450 dev vmgmthost6
+ip link set mtu 1450 dev vmgmtpod6
+ip link set mtu 1450 dev vmgmthost7
+ip link set mtu 1450 dev vmgmtpod7
+ip link set mtu 1450 dev vmgmthost8
+ip link set mtu 1450 dev vmgmtpod8
+ip link set mtu 1450 dev vmgmthost9
+ip link set mtu 1450 dev vmgmtpod9
+ip link set mtu 1450 dev vmgmthost10
+ip link set mtu 1450 dev vmgmtpod10
