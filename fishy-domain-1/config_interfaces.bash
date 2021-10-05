face2aae0da053724da9ae18cabc05c40cfc3cf9
lewisfelix <luisfgon@it.uc3m.es>
2021-10-05 15:44:19 +0000

    Change mgmt terminology

--- fishy-domain-1/config_interfaces.bash
+++ fishy-domain-1/config_interfaces.bash
@@ -8,16 +8,16 @@ ip link add dev vdatapod7 type veth peer name vdatahost7
 ip link add dev vdatapod8 type veth peer name vdatahost8
 ip link add dev vdatapod9 type veth peer name vdatahost9
 ip link add dev vdatapod10 type veth peer name vdatahost10
-ip link add dev vctrlpod1 type veth peer name vctrlhost1
-ip link add dev vctrlpod2 type veth peer name vctrlhost2
-ip link add dev vctrlpod3 type veth peer name vctrlhost3
-ip link add dev vctrlpod4 type veth peer name vctrlhost4
-ip link add dev vctrlpod5 type veth peer name vctrlhost5
-ip link add dev vctrlpod6 type veth peer name vctrlhost6
-ip link add dev vctrlpod7 type veth peer name vctrlhost7
-ip link add dev vctrlpod8 type veth peer name vctrlhost8
-ip link add dev vctrlpod9 type veth peer name vctrlhost9
-ip link add dev vctrlpod10 type veth peer name vctrlhost10
+ip link add dev vmgmtpod1 type veth peer name vmgmthost1
+ip link add dev vmgmtpod2 type veth peer name vmgmthost2
+ip link add dev vmgmtpod3 type veth peer name vmgmthost3
+ip link add dev vmgmtpod4 type veth peer name vmgmthost4
+ip link add dev vmgmtpod5 type veth peer name vmgmthost5
+ip link add dev vmgmtpod6 type veth peer name vmgmthost6
+ip link add dev vmgmtpod7 type veth peer name vmgmthost7
+ip link add dev vmgmtpod8 type veth peer name vmgmthost8
+ip link add dev vmgmtpod9 type veth peer name vmgmthost9
+ip link add dev vmgmtpod10 type veth peer name vmgmthost10
 ip link set mtu 1450 dev vdatahost1
 ip link set mtu 1450 dev vdatapod1
 ip link set mtu 1450 dev vdatahost2
@@ -38,23 +38,23 @@ ip link set mtu 1450 dev vdatahost9
 ip link set mtu 1450 dev vdatapod9
 ip link set mtu 1450 dev vdatahost10
 ip link set mtu 1450 dev vdatapod10
-ip link set mtu 1450 dev vctrlhost1
-ip link set mtu 1450 dev vctrlpod1
-ip link set mtu 1450 dev vctrlhost2
-ip link set mtu 1450 dev vctrlpod2
-ip link set mtu 1450 dev vctrlhost3
-ip link set mtu 1450 dev vctrlpod3
-ip link set mtu 1450 dev vctrlhost4
-ip link set mtu 1450 dev vctrlpod4
-ip link set mtu 1450 dev vctrlhost5
-ip link set mtu 1450 dev vctrlpod5
-ip link set mtu 1450 dev vctrlhost6
-ip link set mtu 1450 dev vctrlpod6
-ip link set mtu 1450 dev vctrlhost7
-ip link set mtu 1450 dev vctrlpod7
-ip link set mtu 1450 dev vctrlhost8
-ip link set mtu 1450 dev vctrlpod8
-ip link set mtu 1450 dev vctrlhost9
-ip link set mtu 1450 dev vctrlpod9
-ip link set mtu 1450 dev vctrlhost10
-ip link set mtu 1450 dev vctrlpod10
+ip link set mtu 1450 dev vmgmthost1
+ip link set mtu 1450 dev vmgmtpod1
+ip link set mtu 1450 dev vmgmthost2
+ip link set mtu 1450 dev vmgmtpod2
+ip link set mtu 1450 dev vmgmthost3
+ip link set mtu 1450 dev vmgmtpod3
+ip link set mtu 1450 dev vmgmthost4
+ip link set mtu 1450 dev vmgmtpod4
+ip link set mtu 1450 dev vmgmthost5
+ip link set mtu 1450 dev vmgmtpod5
+ip link set mtu 1450 dev vmgmthost6
+ip link set mtu 1450 dev vmgmtpod6
+ip link set mtu 1450 dev vmgmthost7
+ip link set mtu 1450 dev vmgmtpod7
+ip link set mtu 1450 dev vmgmthost8
+ip link set mtu 1450 dev vmgmtpod8
+ip link set mtu 1450 dev vmgmthost9
+ip link set mtu 1450 dev vmgmtpod9
+ip link set mtu 1450 dev vmgmthost10
+ip link set mtu 1450 dev vmgmtpod10
