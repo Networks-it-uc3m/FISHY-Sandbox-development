59685d28456f7897eff2c1d0290be9a63143c59d
lewisfelix <43970783+lewisfelix@users.noreply.github.com>
2021-10-11 16:29:22 +0200

    Update config_vxlans.bash

--- fishy-domain-1/config_vxlans.bash
+++ fishy-domain-1/config_vxlans.bash
@@ -1,4 +1,4 @@
-p link add vxlan1 type vxlan id 1969 dev ens3 dstport 4789
+ip link add vxlan1 type vxlan id 1969 dev ens3 dstport 4789
 ip link set vxlan1 up
 bridge fdb append to 00:00:00:00:00:00 dst 10.4.48.195 dev vxlan1
 ip link add vxlan2 type vxlan id 1970 dev ens3 dstport 4789
