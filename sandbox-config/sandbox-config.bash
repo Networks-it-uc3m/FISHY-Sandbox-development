6ab95fc43004f1b11a6f2f377c118d99d30dd5a8
Ubuntu <luisfgon@it.uc3m.es>
2021-11-05 14:42:25 +0000

    Include sandbox config file

---
+++ sandbox-config/sandbox-config.bash
@@ -0,0 +1,121 @@
+#!/bin/bash
+
+while_var=0
+domain_config=""
+
+ifaces=($(ip link show | grep -E "^.:|^..:" | awk '{ print $2 } ' | cut -d ':' -f1 | grep -Ev 'lo'))
+
+for (( c=0; c<${#ifaces[@]}; c++ )); 
+do
+	if [[ $(/sbin/ip route | awk '/default/ { print $5 }') == ${ifaces[c]} ]]; then
+		mainInterface=${ifaces[c]}
+		echo "Building the sandbox using interface $mainInterface"
+		break
+	fi
+	if [ $c -eq $((${#ifaces[@]} - 1)) ]; then
+		mainInterface=${ifaces[0]}
+		echo "WARNING! No default interface configured: Building the sandbox using interface $mainInterface"
+	fi
+done
+
+if [[ $(ip link show | grep -E $mainInterface | awk '{ print $9 }') == "DOWN" ]]; then
+	echo "Interface $mainInterface is in DOWN state: the sandbox installation cannot procceed"
+	exit 1
+fi
+
+
+while [ $while_var -eq 0 ]
+do
+echo "Is this machine fishy-control-services host?[y/n]"
+read control_bool
+
+	if [[ "$control_bool" == "y" ]]; then
+		echo Please, enter Domain 1 IP:
+		read domain1
+		echo Please, enter Domain 2 IP:
+		read domain2
+		domain_config="fishy-control-services"
+		while_var=1
+		sudo ip link add vxlan1 type vxlan id 1969 dev $mainInterface dstport 4789
+		sudo ip link set vxlan1 up
+		sudo bridge fdb append to 00:00:00:00:00:00 dst $domain1 dev vxlan1
+		sudo ip link add vxlan3 type vxlan id 1971 dev $mainInterface dstport 4789
+		sudo ip link set vxlan3 up
+		sudo bridge fdb append to 00:00:00:00:00:00 dst $domain2 dev vxlan3
+		git clone https://github.com/Networks-it-uc3m/FISHY-Sandbox-development.git &> /dev/null
+		sudo $HOME/FISHY-Sandbox-development/$domain_config/config_interfaces.bash
+
+	elif [[ "$control_bool" == "n" ]]; then
+		echo "Is this domain-1 or domain-2?[domain-1/domain-2]"
+		read domain_selector
+		if [[ "$domain_selector" == "domain-1" ]]; then
+			echo Please, enter Control-services IP:
+			read cont
+			echo Please, enter Domain 2 IP:
+			read domain2
+			domain_config="fishy-domain-1"
+			while_var=1
+			sudo ip link add vxlan1 type vxlan id 1969 dev $mainInterface dstport 4789
+			sudo ip link set vxlan1 up
+			sudo bridge fdb append to 00:00:00:00:00:00 dst $cont dev vxlan1
+			sudo ip link add vxlan2 type vxlan id 1970 dev $mainInterface dstport 4789
+			sudo ip link set vxlan2 up
+			sudo bridge fdb append to 00:00:00:00:00:00 dst $domain2 dev vxlan2
+			git clone https://github.com/Networks-it-uc3m/FISHY-Sandbox-development.git &> /dev/null
+			sudo $HOME/FISHY-Sandbox-development/$domain_config/config_interfaces.bash
+
+	        elif [[ "$domain_selector" == "domain-2" ]]; then
+				echo Please, enter Control-services IP:
+				read cont
+				echo Please, enter Domain 1 IP:
+				read domain1
+				domain_config="fishy-domain-2"
+				while_var=1
+				sudo ip link add vxlan2 type vxlan id 1970 dev $mainInterface dstport 4789
+				sudo ip link set vxlan2 up
+				sudo bridge fdb append to 00:00:00:00:00:00 dst $domain1 dev vxlan2
+				sudo ip link add vxlan3 type vxlan id 1971 dev $mainInterface dstport 4789
+				sudo ip link set vxlan3 up
+				sudo bridge fdb append to 00:00:00:00:00:00 dst $cont dev vxlan3
+				git clone https://github.com/Networks-it-uc3m/FISHY-Sandbox-development.git &> /dev/null
+				sudo $HOME/FISHY-Sandbox-development/$domain_config/config_interfaces.bash
+
+		else
+			echo "Invalid Domain: please select an appropiate domain name"
+		fi
+	fi
+done
+
+sudo hostnamectl set-hostname $domain_config
+
+sudo kubeadm init --config $HOME/FISHY-Sandbox-development/clusterConfig.yaml
+
+mkdir -p $HOME/.kube
+sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+sudo chown $(id -u):$(id -g) $HOME/.kube/config
+
+kubectl taint nodes --all node-role.kubernetes.io/master-
+
+kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/kube-flannel.yml
+
+sleep 20
+
+git clone https://github.com/k8snetworkplumbingwg/multus-cni.git &> /dev/null
+kubectl apply -f ./multus-cni/deployments/multus-daemonset.yml
+sudo rm -r multus-cni
+
+sleep 20
+
+kubectl create -f $HOME/FISHY-Sandbox-development/$domain_config/network_definitions/hosts/mgmt_interfaces &> /dev/null
+kubectl create -f $HOME/FISHY-Sandbox-development/$domain_config/network_definitions/pods/mgmt_interfaces &> /dev/null
+kubectl create -f $HOME/FISHY-Sandbox-development/$domain_config/network_definitions/hosts/data_interfaces &> /dev/null
+kubectl create -f $HOME/FISHY-Sandbox-development/$domain_config/network_definitions/pods/data_interfaces &> /dev/null
+kubectl create -f $HOME/FISHY-Sandbox-development/$domain_config/network_definitions/vxlans &> /dev/null
+
+sleep 5
+
+kubectl create -f $HOME/FISHY-Sandbox-development/$domain_config/NED/
+
+sudo rm -r $HOME/FISHY-Sandbox-development
+
+echo "Node $domain_config ready!"
